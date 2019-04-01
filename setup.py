dc2e9831053ffa1a6c3e1d9f95c830c358125978
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-04-01 23:08:39 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,38 +1,13 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
-from imp import load_source
-from os import path
-import io
-
-__version__ = load_source('ishrp_core.version', 'ishrp_core/version.py').__version__
-
-here = path.abspath(path.dirname(__file__))
-
-# get the dependencies and installs
-with io.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
-
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
-
+from distutils.core import setup
 setup(
-    name='isharp_core-jeremycward',
-    author='jeremycward',
-    author_email='jeremy_c_ward@yahoo.co.uk',
-    version=__version__,
-    description='python-seed',
-    url='https://github.com/',
-    license='MIT',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    keywords='',
-    entry_points={
-    },
-    packages=find_packages(exclude=['docs', 'tests*']),
-    include_package_data=True,
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+  name = 'isharp',
+  packages = ['ishrp_core','arctic_broker','arctic_broker.broker_impl','csv_files'],
+  version = '0.3',
+  description = 'ishrp',
+  author = 'jeremycward',
+  author_email = 'jeremycward@yahoo.co.uk',
+  url = 'https://github.com/jeremycward/isharp-core',
+  download_url = 'https://github.com/jeremycward/isharp-core/tarball/0.1',
+  keywords = ['string', 'reverse'],
+  classifiers = [],
 )
