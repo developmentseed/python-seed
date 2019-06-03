6711b6f51f1b35111ee7e55257ce0cc61892a623
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-06-03 22:27:06 +0100

    fixed typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'isharp',
   packages = ['core','arctic_broker','arctic_broker.broker_impl','csv_files'],
   version = '0.0.99',
-  description = 'Algorihtm workbench',
+  description = 'Algorithmic workbench',
   author = 'jeremycward',
   author_email = 'jeremycward@yahoo.co.uk',
   url = 'https://github.com/jeremycward/isharp-core',
