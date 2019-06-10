598a8f2028749e225ca4e02c679c47f4e8ccac50
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-06-10 20:57:01 +0100

    version -> 0.103

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
   name = 'isharp',
-  packages = ['core','arctic_broker','arctic_broker.broker_impl','csv_files'],
-  version = '0.102',
+  packages = ['isharp','arctic_broker','arctic_broker.broker_impl','csv_files'],
+  version = '0.103',
   description = 'Algorithmic workbench',
   author = 'jeremycward',
   author_email = 'jeremycward@yahoo.co.uk',
