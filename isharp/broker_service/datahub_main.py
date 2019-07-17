76cb304e8a6fe53339f6edbda51d65454b5e1e83
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-07-17 11:40:28 +0100

    addded main declaration

--- isharp/broker_service/datahub_main.py
+++ isharp/broker_service/datahub_main.py
@@ -4,7 +4,7 @@ import re
 import sys
 import isharp.yaml_support  as iYaml
 import nameko.cli.main
-if __name__ == '__main__':
+def main():
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     iYaml.set_up_unsafe_loader()
     sys.exit(nameko.cli.main.main())
