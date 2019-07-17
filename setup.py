ca0c871028d4e950b0a8d980284774d988322af8
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-07-17 10:48:53 +0100

    modified setup.py to create executable entry point

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
 from setuptools import find_packages
+
 setup(
-  name = 'isharp',
-  packages = find_packages(),
-  version = '1.3',
-  description = 'Algorithmic workbench',
-  author = 'jeremycward',
-  author_email = 'jeremycward@yahoo.co.uk',
-  url = 'https://github.com/jeremycward/isharp-core',
-  download_url = 'https://github.com/jeremycward/isharp-core/tarball/1.00',
-  keywords = ['string', 'reverse'],
-  classifiers = [],
+    name='isharp',
+    packages=find_packages(),
+    version='1.3',
+    description='Algorithmic workbench',
+    author='jeremycward',
+    author_email='jeremycward@yahoo.co.uk',
+    url='https://github.com/jeremycward/isharp-core',
+    download_url='https://github.com/jeremycward/isharp-core/tarball/1.00',
+    keywords=['string', 'reverse'],
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+            'datahub=isharp.broker_service.datahub_main:main',
+        ]
+    },
 )
