1e0d2de649d61e9cefc6b5e6ab5cac95c71fd087
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-10-05 11:20:49 +0100

    added run_time scripts

--- dev_scripts/setup.sh
+++ dev_scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-su -l ec2-user
+echo PYTHONPATH=/home/ec2-user/isharp-core >> /etc/environment
 yum update -y
 yum install -y python37
 yum install -y python-pip
@@ -7,11 +7,10 @@ yum install -y git
 yum install -y docker
 usermod -a -G docker ec2-user
 curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-isharp-core
 chmod +x /usr/local/bin/docker-compose
 service docker start
 chkconfig docker on
 yum update -y
 cd ~ec2-user
 git clone https://github.com/jeremycward/isharp-core.git
-export PYTHONPATH = /home/ec2-user/isharp-core
+
