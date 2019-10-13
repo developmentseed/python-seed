3e085d14d3880a182c372a9e9e8ea91b6ecb5639
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-10-13 11:08:03 +0100

    read init file from cmd line

--- dev_scripts/init_mongo.sh
+++ dev_scripts/init_mongo.sh
@@ -1,4 +1,4 @@
-python /isharp-core/demo_data/mongo/arctic_demo_init/init_demo_db.py
+python /isharp-core/demo_data/mongo/arctic_demo_init/init_demo_db.py /isharp-core/demo_data/mongo/arctic_demo_init/arctic_demo.yaml
 
 
 
