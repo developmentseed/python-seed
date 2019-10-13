fa01f2ae7d71211a9f538332578990d77512a572
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-10-13 09:38:07 +0100

    start up script for dev

---
+++ dev_scripts/run_webserver.sh
@@ -0,0 +1,1 @@
+ python ../isharp/datahub/broker_service/datahub_webserver_main.py
--- dev_scripts/rundatahub.sh
+++ dev_scripts/rundatahub.sh
@@ -1,1 +1,2 @@
+
 python3 ../isharp/datahub/broker_service/datahub_main.py run --config ./config.yaml  isharp.datahub.broker_service.server:DataBrokerService
