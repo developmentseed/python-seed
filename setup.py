59cc670df6ac8ca1bbf9de72f8a075c7000ce62f
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-11-17 21:13:43 +0000

    version -> 2.60 changed ordering of requirements.txt

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='isharp',
     packages=find_packages(),
-    version='2.59',
+    version='2.60',
     description='Algorithmic workbench',
     author='jeremycward',
     author_email='jeremycward@yahoo.co.uk',
