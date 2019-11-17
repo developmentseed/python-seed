57a1985fcff952a95fed66033ff1b78edc6565c1
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-11-17 11:10:43 +0000

    removed annoying fill

--- docker_images/datahub/init.sh
+++ docker_images/datahub/init.sh
@@ -3,6 +3,6 @@ echo STARTING DATAHUB:
 env
 echo =======================================================================================
 
-datahub run --config ./config.yaml isharp.datahub.broker_service.server:DataBrokerService
+datahub run --config ./config.yaml isharp.datahub.broker_service.server:DataBrokerService &
 
-web
+web &
