360d4725a38217459077678638d7c785cc2442bd
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-11-18 20:51:30 +0000

    upped version to 2.62

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='isharp',
     packages=find_packages(),
-    version='2.61',
+    version='2.62',
     description='Algorithmic workbench',
     author='jeremycward',
     author_email='jeremycward@yahoo.co.uk',
