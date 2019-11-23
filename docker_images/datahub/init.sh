7acdf0682ac6b051ad8b9795418487d5dff9ef6b
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2019-11-23 20:30:19 +0000

    added jupyter with correct params to init

--- docker_images/datahub/init.sh
+++ docker_images/datahub/init.sh
@@ -12,7 +12,7 @@ echo ===========================================================================
 
 
 datahub run --config ./config.yaml isharp.datahub.broker_service.server:DataBrokerService &
-jupyter notebook &
+jupyter notebook --ip=0.0.0.0  --allow-root &
 web
 
 
