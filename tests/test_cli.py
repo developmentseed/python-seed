cf88f85efdfe50743f3246a5f523b61331da98bf
vincentsarago <vincent.sarago@gmail.com>
2020-08-21 12:51:04 -0400

    moar tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 """tests python_seed.cli."""
 
+import os
 
 from click.testing import CliRunner
 
@@ -11,6 +12,17 @@ def test_create():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(pyseed, ["create", "myfunction"])
+        assert not os.path.exists("myfunction/.github/workflows/ci.yml")
+        assert not os.path.exists("myfunction/codecov.yml")
+        with open("myfunction/README.md", "r") as f:
+            assert f.read().splitlines()[0] == "# myfunction"
+        assert not result.exception
+        assert result.exit_code == 0
+
+    with runner.isolated_filesystem():
+        result = runner.invoke(pyseed, ["create", "myfunction", "--ci", "github"])
+        assert os.path.exists("myfunction/.github/workflows/ci.yml")
+        assert os.path.exists("myfunction/codecov.yml")
         with open("myfunction/README.md", "r") as f:
             assert f.read().splitlines()[0] == "# myfunction"
         assert not result.exception
