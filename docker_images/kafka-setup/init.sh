5578e95bffdf8db7971ae928badd0e0f181bab68
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2020-12-31 20:02:04 +0000

    fixed typo in http path .. added sleeps

--- docker_images/kafka-setup/init.sh
+++ docker_images/kafka-setup/init.sh
@@ -6,7 +6,15 @@ kafka-topics --create --if-not-exists --zookeeper zookeeper:2181 --partitions 1 
 echo topics created OK
 echo Waiting for queues to be created
 
-curl -X POST http://connect:8083//connectors -d @rabbit_request.json --header  "Content-Type: application/json"
+# wait for queues to be created.
+sleep 20
+
+curl -X POST http://connect:8083/connectors -d @rabbit_request.json --header  "Content-Type: application/json"
+
+# wait for connectors to be established
+sleep 20
+
+curl  http://connect:8083/connectors
 
 
 while :; do sleep 2073600; done
