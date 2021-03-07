6feeb845323f72ba02edec965854f9f4b33b45e6
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2021-03-07 20:37:23 +0000

    changed bincommand

--- isharp/poloniex/pol_sub2.py
+++ isharp/poloniex/pol_sub2.py
@@ -3,30 +3,36 @@ import threading
 import time
 import json
 import pika
-from datetime import datetime
+from datetime import datetime, timezone,timedelta
+
 start_millis = int(round(time.time() * 1000))
 msg_key = start_millis
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='confluent'))
+offset =  timezone(timedelta(hours=0))
+format = "%Y-%m-%dT%H:%M:%S.%f %Z"
 
 
 def on_message(ws, message):
+    now = datetime.now(offset)
     props = pika.BasicProperties(
 
-                                 app_id="poloniex",
-                                 type="ticker.data",
+                                 app_id="polozilla",
+                                 type="classic",
                                  user_id="guest",
                                  content_encoding="UTF-16",
                                  content_type="application/octet",
                                  delivery_mode=2,
                                  expiration="60000",
                                  headers=[],
-
                                  priority=1,
-                                 reply_to="n/a"
+                                 reply_to="n/a",
+
     )
 
     if (message!='[1002,1]'):
-      channel.basic_publish(exchange='', routing_key="pol.tickers", body=message,properties=props)
+      msg_with_timestamp =   '{}{}'.format(now.strftime(format), message)
+      #print(msg_with_timestamp)
+      channel.basic_publish(exchange='', routing_key="pol.tickers", body=msg_with_timestamp,properties=props)
 
 def on_error(ws, error):
     print(error)
@@ -44,7 +50,7 @@ def on_open(ws):
         # ws.send(json.dumps({'command':'subscribe','channel':1003}))
         # ws.send(json.dumps({'command':'subscribe','channel':1010}))
         # ws.send(json.dumps({'command':'subscribe','channel':'BTC_XMR'}))
-        time.sleep(180)
+        time.sleep(1800)
         ws.close()
         print("thread terminating...")
     threading.Thread(target = run).start()
@@ -61,3 +67,5 @@ if __name__ == "__main__":
                               on_close = on_close)
     ws.on_open = on_open
     ws.run_forever()
+
+
